0bf1d36cc375efb02fa7ea670cd47f549c9e36e2
Shashwat Mookherjee <67093310+sm1612@users.noreply.github.com>
2023-09-09 21:10:11 +0530

    main.py

--- main.py
+++ main.py
@@ -1,71 +1,69 @@
 import cv2
 import mediapipe as mp
-import time
 
-# Initialize MediaPipe Face Detection
+# Initialize MediaPipe Face Detection and Face Mesh
 mp_face_detection = mp.solutions.face_detection
+mp_face_mesh = mp.solutions.face_mesh
 mp_drawing = mp.solutions.drawing_utils
 
 # Initialize webcam
 cap = cv2.VideoCapture(0)
 
-# Create a dictionary to store registered users and their attendance times
-registered_users = {}
-current_user = None
-start_time = None
+# Create a dictionary to store unique IDs for each detected face
+face_ids = {}
+next_id = 1
 
+# Initialize MediaPipe Face Detection
 with mp_face_detection.FaceDetection(
     min_detection_confidence=0.5) as face_detection:
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            continue
+    # Initialize MediaPipe Face Mesh for full face and eye tracking
+    with mp_face_mesh.FaceMesh(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as face_mesh:
 
-        # Convert BGR image to RGB
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
 
-        # Perform face detection
-        results = face_detection.process(frame_rgb)
+            # Convert BGR image to RGB
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        if results.detections:
-            for detection in results.detections:
-                bboxC = detection.location_data.relative_bounding_box
-                ih, iw, _ = frame.shape
-                x, y, w, h = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
-                             int(bboxC.width * iw), int(bboxC.height * ih)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # Perform face detection
+            results_face = face_detection.process(frame_rgb)
 
-                # If a user is registered, display their name above the bounding box
-                if current_user:
-                    cv2.putText(frame, current_user, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            # Perform face mesh detection for full face and eye tracking
+            results_mesh = face_mesh.process(frame_rgb)
 
-                # Check if a new user needs to be registered
-                if current_user is None:
-                    cv2.putText(frame, "Register your name and press 'Enter'", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            if results_face.detections:
+                for i, detection in enumerate(results_face.detections):
+                    bboxC = detection.location_data.relative_bounding_box
+                    ih, iw, _ = frame.shape
+                    x, y, w, h = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                                int(bboxC.width * iw), int(bboxC.height * ih)
 
-        cv2.imshow('Attendance System', frame)
+                    # Generate a unique ID for this face
+                    if i not in face_ids:
+                        face_ids[i] = next_id
+                        next_id += 1
 
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
-        elif key == 13:  # Enter key is pressed to register a new user
-            if current_user is None:
-                current_user = input("Enter your name: ")
-                start_time = time.time()
-                registered_users[current_user] = 0
-                print(f"Registered {current_user} for attendance.")
-        elif key == 27:  # Esc key is pressed to unregister the current user
-            if current_user:
-                end_time = time.time()
-                registered_users[current_user] += end_time - start_time
-                print(f"{current_user}'s attendance duration: {registered_users[current_user]:.2f} seconds")
-                current_user = None
+                    # Display the ID and bounding box around the face
+                    cv2.putText(frame, f"ID: {face_ids[i]}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            if results_mesh and results_mesh.multi_face_landmarks:
+                for face_landmarks in results_mesh.multi_face_landmarks:
+                    for id, lm in enumerate(face_landmarks.landmark):
+                        ih, iw, _ = frame.shape
+                        x, y = int(lm.x * iw), int(lm.y * ih)
+                        cv2.circle(frame, (x, y), 2, (0, 0, 255), -1)
+
+            cv2.imshow('Face and Eye Tracking', frame)
+
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
 
 cap.release()
 cv2.destroyAllWindows()
-
-# Print the final attendance record
-print("\nAttendance Record:")
-for user, duration in registered_users.items():
-    print(f"{user}: {duration:.2f} seconds")
